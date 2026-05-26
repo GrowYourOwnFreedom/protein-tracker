b1e57e53c801bb9cf405fedabb857a6a122bbc98
GrowYourOwnFreedom <bigbelchis@hotmail.com>
2026-05-26 17:05:15 +0100

    create test server with basic CRUD routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import type { Request, Response } from "express";
+import itemRouter from "./routes/itemRoutes.js";
 
 const app = express();
 
@@ -11,27 +12,10 @@ app.get("/", (_request: Request, response: Response) => {
     response.json("server is running");
 });
 
-app.get("/items",(_request:Request,response:Response
-)=>{
-    response.json(testData)
-})
+app.use("/items", itemRouter)
 
-app.post("/items", (request: Request, response: Response)=>{
-    const newItem = {
-        id:Date.now(),
-        name:request.body.name
-    }
-    testData.push(newItem)
-    response.status(201).json(newItem)
-
-})
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`);
 });
 
-const testData = [
-    { id: 1, name: "A" },
-    { id: 2, name: "B" },
-    { id: 3, name: "C" },
-];
 
