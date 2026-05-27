c9093756400cb463870911043bd64c0448e41c07
GrowYourOwnFreedom <bigbelchis@hotmail.com>
2026-05-27 13:52:15 +0100

    add CORS and corresponding .env CLIENT_ORIGIN  variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,24 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import type { Request, Response } from "express";
 import itemRouter from "./routes/itemRoutes.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
 
+app.use(cors({ origin: process.env.CLIENT_ORIGIN }));
 app.use(express.json());
 
-
-const PORT = 3000;
+const PORT = process.env.PORT ?? 3000;
 
 app.get("/", (_request: Request, response: Response) => {
     response.json("server is running");
 });
 
-app.use("/items", itemRouter)
-app.use(errorHandler)
+app.use("/items", itemRouter);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`);
 });
-
-
