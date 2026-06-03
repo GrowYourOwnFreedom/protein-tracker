fe20fb2bc2f64b6e82ffaea9618135736de925f0
GrowYourOwnFreedom <bigbelchis@hotmail.com>
2026-06-04 00:21:50 +0100

    add test for backup data validation helper

---
+++ server/src/__tests__/apiResponseHelpers.test.ts
@@ -0,0 +1,27 @@
+import { createSuccessResponse } from "@/helpers/apiResponseHelpers.js";
+import { describe, expect, it } from "vitest";
+
+describe("createSuccessResonse", () => {
+    it("creates a success response without a message", () => {
+        const data = { status: "ok" };
+        const successResponse = createSuccessResponse(data);
+
+        expect(successResponse).toEqual({
+            success: true,
+            data: {
+                status: "ok",
+            },
+        });
+    });
+
+    it("creates a success response with a message", () => {
+        const data = { id: "item-1", name: "test-item" };
+        const successResponse = createSuccessResponse(data , "Item successfully deleted");
+
+        expect(successResponse).toEqual({
+            success:true,
+            message: "Item successfully deleted",
+            data:{ id: "item-1", name: "test-item" }
+        })
+    });
+});
---
+++ server/src/__tests__/appDataValidationHelpers.test.ts
@@ -0,0 +1,43 @@
+import type { AppDataBackup } from "@/types.js";
+import { isAppDataBackup } from "@/helpers/appDataValidationHelpers.js";
+import { describe, expect, it } from "vitest";
+
+describe("iaAppDataBackup", () => {
+    it("returns true when passed a valid backup", () => {
+        const testBackup: AppDataBackup = {
+            calorieLimit: 2000,
+            proteinTarget: 130,
+            entries: [],
+            meals: [],
+            ingredients: [],
+        };
+        expect(isAppDataBackup(testBackup)).toBe(true);
+    });
+    it("returns false when bad data is passed", () => {
+        expect(isAppDataBackup(null)).toBe(false);
+        expect(
+            isAppDataBackup({
+                calorieLimit: "2000",
+                proteinTarget: "130",
+                entries: [],
+                meals: [],
+                ingredients: [],
+            }),
+        ).toBe(false);
+        expect(
+            isAppDataBackup({
+                entries: [],
+                meals: [],
+                ingredients: [],
+            }),
+        ).toBe(false);
+        expect(isAppDataBackup({
+            calorieLimit: 2000,
+            proteinTarget: 130,
+            entries: [],
+            meals: [],
+            ingredients: {},
+        })).toBe(false)
+        expect(isAppDataBackup("data")).toBe(false)
+    });
+});
